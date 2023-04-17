47ae101290b054b24c5e89d0e8527e6136942d9c
Exedward <eduardo.ex7@gmail.com>
2023-04-17 01:28:46 -0300

    m3

--- testeBot_v2/testeBot_v2.ino
+++ testeBot_v2/testeBot_v2.ino
@@ -1,15 +1,19 @@
+//Funcionando
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <MediaMovel.h>
+#include <Edu_Ultrassonico.h>
 
 WiFiClient client;
 HTTPClient http;
-
-#define   PIN_BT    D2
+Ultrassonico  ultra(D1, D2); //Echo, Trigger
+MM  mms(10);
 
 const char* ssid = "TP LINK";
 const char* password = "morais1212";
-bool statusBt = 0;
-unsigned long lastTime = 0;
+bool statusDetect = 0;
+unsigned long lastTime = 0, cm = 10, timeIntervalUltra = 0;
+float distancia = 0;
 
 String apiKey = "3466759";
 String whats = "558597498597";
@@ -17,13 +21,26 @@ String messageToSend = "Botão+1+Acionado.";
 
 void setup(){
   Serial.begin(115200);
-  pinMode(PIN_BT, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
   setup_wifi();
 }
 void loop(){
-  if(!digitalRead(PIN_BT) && !statusBt){
+  if(millis() - timeIntervalUltra >= 60){
+    timeIntervalUltra = millis();
+    cm = ultra.centimetrosUltra();
+    distancia = mms.calculaMMS(cm);
+    Serial.print("Distancia: ");
+    Serial.println(distancia);
+  }
+  if(distancia <= 5) digitalWrite(LED_BUILTIN, 0);
+  else digitalWrite(LED_BUILTIN, 1);
+  if((distancia > 5) && statusDetect){
+    statusDetect = 0;
+  } 
+  if((distancia <= 5) && !statusDetect){
+    statusDetect = 1;
     lastTime = millis();
-    statusBt = 1;
+    
     if(WiFi.status() == WL_CONNECTED){
       String requestUrl = "http://api.callmebot.com/whatsapp.php?phone="+whats+"&text="+messageToSend+"&apikey="+apiKey; 
       http.begin(client, requestUrl);
@@ -47,13 +64,9 @@ void loop(){
       Serial.println("WiFi Desconectado");
     }
   }
-  if(statusBt) if(millis() - lastTime >= 1000) statusBt = 0;
 }
 
 void setup_wifi(){
-  delay(10);
-
-  Serial.println();
   Serial.print("Conectando-se a ");
   Serial.println(ssid);
 
